6f0635b6b43410a8d9f477f76f31d63439fb9ad6
George <gwwebster23@gmail.com>
2023-04-19 13:17:49 -0500

    add form submission event

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const form = document.querySelector('.form');
 const estimationResult = document.querySelector('#estimation');
 const searchLog = document.querySelector('#log');
 const lightDark = document.querySelector('#light_dark');
+
+//form submission
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault()
+    handleFetch()
+};
