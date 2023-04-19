a015e6a3406ed57dfb8c7d72b7a56a862cd4a7c1
George <gwwebster23@gmail.com>
2023-04-19 13:02:49 -0500

    add global variables

---
+++ index.js
@@ -0,0 +1,5 @@
+const fundDropdown = document.querySelector('#fund_dropdown');
+const form = document.querySelector('.form');
+const estimationResult = document.querySelector('#estimation');
+const searchLog = document.querySelector('#log');
+const lightDark = document.querySelector('#light_dark');
